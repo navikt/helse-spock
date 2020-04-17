acad18f0bc83ee28cd6940ad149ec37dfb493e02
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-04-17 09:14:33 +0200

    Fjerner transasksjoner fra databasekall som ikke trenger det

--- spock/src/main/kotlin/no/nav/helse/spock/SpesialistPåminnelseDao.kt
+++ spock/src/main/kotlin/no/nav/helse/spock/SpesialistPåminnelseDao.kt
@@ -10,68 +10,62 @@ import javax.sql.DataSource
 
 class SpesialistPåminnelseDao(private val dataSource: DataSource) {
 
-    fun finnEllerOpprettPåminnelse(referanse: String, fødselsnummer: String, endringstidspunkt: LocalDateTime, timeout: Long) {
+    fun finnEllerOpprettPåminnelse(
+        referanse: String,
+        fødselsnummer: String,
+        endringstidspunkt: LocalDateTime,
+        timeout: Long
+    ) {
         using(sessionOf(dataSource)) { session ->
-            session.transaction {
-                val påminnelse = finnPåminnelse(referanse)
-                val nestePåminnelse = endringstidspunkt.plusSeconds(timeout)
-                if (påminnelse == null) {
-                    opprettPåminnelse(referanse, fødselsnummer, endringstidspunkt, timeout, nestePåminnelse)
-                } else {
-                    oppdaterPåminnelse(påminnelse.id, endringstidspunkt, timeout, nestePåminnelse)
-                }
+            val påminnelse = finnPåminnelse(referanse)
+            val nestePåminnelse = endringstidspunkt.plusSeconds(timeout)
+            if (påminnelse == null) {
+                opprettPåminnelse(referanse, fødselsnummer, endringstidspunkt, timeout, nestePåminnelse)
+            } else {
+                oppdaterPåminnelse(påminnelse.id, endringstidspunkt, timeout, nestePåminnelse)
             }
         }
     }
 
-    fun finnPåminnelse(referanse: String) = using(sessionOf(dataSource)) { session ->
-        session.transaction { tx ->
-            tx.run(
-                    queryOf("SELECT * FROM spesialist_paminnelse WHERE referanse=?;", referanse)
-                            .map(::toDTO).asSingle
-            )
-
-        }
+    private fun finnPåminnelse(referanse: String) = using(sessionOf(dataSource)) { session ->
+        session.run(
+            queryOf("SELECT * FROM spesialist_paminnelse WHERE referanse=?;", referanse)
+                .map(::toDTO).asSingle
+        )
     }
 
-    fun opprettPåminnelse(
-            referanse: String,
-            fødselsnummer: String,
-            endringstidspunkt: LocalDateTime,
-            timeout: Long,
-            nestePåminnelse: LocalDateTime
+    private fun opprettPåminnelse(
+        referanse: String,
+        fødselsnummer: String,
+        endringstidspunkt: LocalDateTime,
+        timeout: Long,
+        nestePåminnelse: LocalDateTime
     ) = using(sessionOf(dataSource)) { session ->
-        session.transaction { tx ->
-            tx.run(
-                    queryOf(
-                            "INSERT INTO spesialist_paminnelse(fodselsnummer, referanse, endringstidspunkt, timeout, neste_paminnelsetidspunkt) VALUES(?, ?, ?, ?, ?);",
-                            fødselsnummer, referanse, endringstidspunkt, timeout, nestePåminnelse
-                    ).asUpdate
-            )
-        }
+        queryOf(
+            "INSERT INTO spesialist_paminnelse(fodselsnummer, referanse, endringstidspunkt, timeout, neste_paminnelsetidspunkt) VALUES(?, ?, ?, ?, ?);",
+            fødselsnummer, referanse, endringstidspunkt, timeout, nestePåminnelse
+        ).asUpdate
     }
 
-    fun oppdaterPåminnelse(
-            påminnelseId: Long,
-            endringstidspunkt: LocalDateTime,
-            timeout: Long,
-            nestePåminnelse: LocalDateTime
+    private fun oppdaterPåminnelse(
+        påminnelseId: Long,
+        endringstidspunkt: LocalDateTime,
+        timeout: Long,
+        nestePåminnelse: LocalDateTime
     ) = using(sessionOf(dataSource)) { session ->
-        session.transaction { tx ->
-            tx.run(
-                    queryOf(
-                            "UPDATE spesialist_paminnelse SET endringstidspunkt=?, timeout=?, neste_paminnelsetidspunkt=? WHERE id=?;",
-                            endringstidspunkt, timeout, nestePåminnelse, påminnelseId
-                    ).asUpdate
-            )
-        }
+        session.run(
+            queryOf(
+                "UPDATE spesialist_paminnelse SET endringstidspunkt=?, timeout=?, neste_paminnelsetidspunkt=? WHERE id=?;",
+                endringstidspunkt, timeout, nestePåminnelse, påminnelseId
+            ).asUpdate
+        )
     }
 
     fun hentPåminnelser(): List<SpesialistPåminnelseDto> = using(sessionOf(dataSource)) { session ->
         session.transaction { tx ->
             tx.run(
-                    queryOf("SELECT * FROM spesialist_paminnelse WHERE now() > neste_paminnelsetidspunkt;")
-                            .map(::toDTO).asList
+                queryOf("SELECT * FROM spesialist_paminnelse WHERE now() > neste_paminnelsetidspunkt;")
+                    .map(::toDTO).asList
             ).also { påminnelser ->
                 påminnelser.forEach {
                     oppdaterTidspunkt(tx, it)
@@ -82,21 +76,21 @@ class SpesialistPåminnelseDao(private val dataSource: DataSource) {
 
     private fun toDTO(it: Row): SpesialistPåminnelseDto {
         return SpesialistPåminnelseDto(
-                it.long("id"),
-                it.string("referanse"),
-                it.string("fodselsnummer"),
-                it.int("antall_ganger_paminnet"),
-                it.localDateTime("endringstidspunkt"),
-                it.long("timeout")
+            it.long("id"),
+            it.string("referanse"),
+            it.string("fodselsnummer"),
+            it.int("antall_ganger_paminnet"),
+            it.localDateTime("endringstidspunkt"),
+            it.long("timeout")
         )
     }
 
     private fun oppdaterTidspunkt(tx: TransactionalSession, dto: SpesialistPåminnelseDto) {
         tx.run(
-                queryOf(
-                        "UPDATE spesialist_paminnelse SET neste_paminnelsetidspunkt = (now() + timeout * interval '1 second'), antall_ganger_paminnet = antall_ganger_paminnet + 1 WHERE id=?::BIGINT;",
-                        dto.id
-                ).asUpdate
+            queryOf(
+                "UPDATE spesialist_paminnelse SET neste_paminnelsetidspunkt = (now() + timeout * interval '1 second'), antall_ganger_paminnet = antall_ganger_paminnet + 1 WHERE id=?::BIGINT;",
+                dto.id
+            ).asUpdate
         )
     }
 }
