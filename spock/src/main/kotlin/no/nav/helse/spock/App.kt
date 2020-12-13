280cea9ebebcb018c3a7668d5ac6c42e37e1d498
David Steinsland <david@steinsland.email>
2020-12-13 11:53:50 +0100

    Revert "egen rapid for å lese inn tidligere meldinger"
    This reverts commit 7b1c3b465cc8f0f798eefde0bbd6e166980c5ec6.

--- spock/src/main/kotlin/no/nav/helse/spock/App.kt
+++ spock/src/main/kotlin/no/nav/helse/spock/App.kt
@@ -1,17 +1,9 @@
 package no.nav.helse.spock
 
-import com.fasterxml.jackson.databind.JsonNode
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import no.nav.helse.rapids_rivers.*
+import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
-import java.io.File
-import java.io.FileNotFoundException
 import java.time.Duration
-import javax.sql.DataSource
-import kotlin.coroutines.CoroutineContext
 
 private val log = LoggerFactory.getLogger("no.nav.helse.Spock")
 
@@ -26,8 +18,6 @@ fun launchApp(env: Map<String, String>) {
         ?: Duration.ofMinutes(1)
 
     log.info("Lager påminnelser ca. hver ${schedule.toSeconds()} sekunder")
-
-    seedApp(dataSource, env)
 
     RapidApplication.create(env).apply {
         Tilbakerulling(this, dataSource)
@@ -48,39 +38,5 @@ fun launchApp(env: Map<String, String>) {
     }.start()
 }
 
-private fun seedApp(dataSource: DataSource, env: Map<String, String>) {
-    val kafkaConfig = KafkaConfig(
-        bootstrapServers = env.getValue("KAFKA_BOOTSTRAP_SERVERS"),
-        consumerGroupId = env.getValue("KAFKA_CONSUMER_GROUP_ID") + "-reseed",
-        username = "/var/run/secrets/nais.io/service_user/username".readFile(),
-        password = "/var/run/secrets/nais.io/service_user/password".readFile(),
-        truststore = env["NAV_TRUSTSTORE_PATH"],
-        truststorePassword = env["NAV_TRUSTSTORE_PASSWORD"],
-        autoOffsetResetConfig = "earliest"
-    )
-    val seedApp = KafkaRapid.create(kafkaConfig, env.getValue("KAFKA_RAPID_TOPIC"))
-        .apply {
-            Runtime.getRuntime().addShutdownHook(Thread(this::stop))
-            River(this).apply {
-                validate {
-                    it.requireAny("@event_name", listOf("vedtaksperiode_endret", "utbetaling_endret"))
-                    it.requireKey("fødselsnummer", "aktørId")
-                    it.require("@opprettet", JsonNode::asLocalDateTime)
-                }
-                register(object : River.PacketListener {
-                    override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
-                        lagrePerson(dataSource, packet["fødselsnummer"].asText(), packet["aktørId"].asText(), packet["@opprettet"].asLocalDateTime())
-                    }
-                })
-            }
-        }
 
-   GlobalScope.launch { seedApp.start() }
-}
 
-private fun String.readFile() =
-    try {
-        File(this).readText(Charsets.UTF_8)
-    } catch (err: FileNotFoundException) {
-        null
-    }
