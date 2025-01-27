ef55bed66219dd7332aa918d4ddcd057ba0fd417
david steinsland <david@davidsteinsland.net>
2025-01-27 13:08:44 +0100

    bumper r&r
    se commit for viktig endring:
    https://github.com/navikt/tbd-libs/commit/17a27722b0843e4c1b3cd16aeada837126b6124b

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-val rapidsAndRiversVersion = "2024120608421733470968.fc0a3d5e3685"
+val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
 val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
 val flywayVersion = "10.21.0"
 val postgresqlVersion = "42.7.4"
