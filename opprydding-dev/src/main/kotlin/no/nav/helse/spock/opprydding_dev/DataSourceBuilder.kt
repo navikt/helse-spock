656b2d1531fcf09f908b47a6ff35a2e94e14246e
Sivert Lunsæter <71376389+sivertflu@users.noreply.github.com>
2026-09-18 16:20:14 +0200

    :bug: Les db configs i koden fra riktige env variabler

--- opprydding-dev/src/main/kotlin/no/nav/helse/spock/opprydding_dev/DataSourceBuilder.kt
+++ opprydding-dev/src/main/kotlin/no/nav/helse/spock/opprydding_dev/DataSourceBuilder.kt
@@ -6,14 +6,14 @@ import java.time.Duration
 
 internal class DataSourceBuilder(env: Map<String, String>) {
     private val hikariConfig = HikariConfig().apply {
-        jdbcUrl = env["DATABASE_JDBC_URL"] ?: String.format(
+        jdbcUrl = env["DATABASE_SPOCK_OPPRYDDING_DEV_JDBC_URL"] ?: String.format(
             "jdbc:postgresql://%s:%s/%s",
-            requireNotNull(env["DATABASE_HOST"]) { "database host must be set if jdbc url is not provided" },
-            requireNotNull(env["DATABASE_PORT"]) { "database port must be set if jdbc url is not provided" },
-            requireNotNull(env["DATABASE_DATABASE"]) { "database name must be set if jdbc url is not provided" },
+            requireNotNull(env["DATABASE_SPOCK_OPPRYDDING_DEV_HOST"]) { "database host must be set if jdbc url is not provided" },
+            requireNotNull(env["DATABASE_SPOCK_OPPRYDDING_DEV_PORT"]) { "database port must be set if jdbc url is not provided" },
+            requireNotNull(env["DATABASE_SPOCK_OPPRYDDING_DEV_DATABASE"]) { "database name must be set if jdbc url is not provided" },
         )
-        username = requireNotNull(env["DATABASE_USERNAME"]) { "databasebrukernavn må settes" }
-        password = requireNotNull(env["DATABASE_PASSWORD"]) { "databasepassord må settes" }
+        username = requireNotNull(env["DATABASE_SPOCK_OPPRYDDING_DEV_USERNAME"]) { "databasebrukernavn må settes" }
+        password = requireNotNull(env["DATABASE_SPOCK_OPPRYDDING_DEV_PASSWORD"]) { "databasepassord må settes" }
         maximumPoolSize = 3
         connectionTimeout = Duration.ofSeconds(30).toMillis()
         maxLifetime = Duration.ofMinutes(30).toMillis()
